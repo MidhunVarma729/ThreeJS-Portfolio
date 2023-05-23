fd8c03d504d962b9bdb97f9204400e710b0df93b
MidhunVarma729 <midhun5varma2@gmail.com>
2023-05-23 13:48:27 +0530

    another directory modification

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import '../assets/css/style.css'
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three';
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls';
 
 //  Three most important compontent: Scene, camera, renderer.
